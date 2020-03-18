5481f016e6dcb701bfbce53587af1370d05edacf
Dmitry Khramtsov <hramcovdv@gmail.com>
2020-03-18 13:51:04 +0300

    add statistics and logging

--- service.py
+++ service.py
@@ -6,6 +6,7 @@ import signal
 import logging
 from dotenv import load_dotenv
 from pymongo import MongoClient
+from mongoqueue import MongoQueue
 
 from worker import Worker
 
@@ -16,10 +17,15 @@ load_dotenv()
 mongo = MongoClient(os.getenv('MONGO_URL'))
 db = mongo[os.getenv('MONGO_DB')]
 
+# Init queue in MongoDB server
+queue = MongoQueue(collection=db.tasks,
+                   consumer_id=os.getenv('CONSUMER'),
+                   max_attempts=1)
+
 # init logging config
-logging.basicConfig(filename='messages.log',
+logging.basicConfig(filename=os.getenv('LOGGING'),
                     format='%(asctime)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
+                    level=logging.WARNING)
 
 class ServiceExit(Exception):
     """
@@ -34,6 +40,20 @@ def service_shutdown(signum, frame):
     print(f'\nCaught signal {signum}')
     raise ServiceExit
 
+def statistics(queue, interval=1):
+    """ Statistic generator
+    """
+    previous_count = 0
+    
+    while True:
+        current_count = queue.size()
+        performance = round((previous_count - current_count) / interval)
+
+        yield f'Jobs left: {current_count}, Performance: {performance} jobs/sec'
+
+        time.sleep(interval)
+        previous_count = current_count
+
 def main():
     """ Main service
     """
@@ -47,15 +67,21 @@ def main():
     workers = []
         
     for _ in range(int(os.getenv('THREADS'))):
-        worker = Worker(db)
+        worker = Worker(queue)
         worker.start()
         workers.append(worker)
 
-    try:       
+    try:
+        # Repair locked jobs
+        queue.repair()
+        
+        # Init status generator
+        status = statistics(queue)
+        
         # Keep the main thread running,
         # otherwise signals are ignored.
         while any([w.is_alive() for w in workers]):
-            time.sleep(0.5)
+            print('{:<80}'.format(next(status)), end='\r')
     except ServiceExit:
         # Terminate the running threads.
         # Set the shutdown flag on each thread to
@@ -67,7 +93,7 @@ def main():
         print(f'Main service ended with: {err}')
     finally:
         mongo.close()
-        
+
     print('Stopping main service')
 
 if __name__ == '__main__':
--- worker.py
+++ worker.py
@@ -2,47 +2,44 @@ import time
 import logging
 import threading
 from easysnmp import Session
-from mongoqueue import MongoQueue
 
 class Worker(threading.Thread):
     """ A worker class
     """
-    def __init__(self, database):
+    def __init__(self, queue, timeout=0.5):
         # ... Thread init ...
         threading.Thread.__init__(self, daemon=True)
-
         # The shutdown_flag is a threading.Event object that
         # indicates whether the thread should be terminated.
         self.shutdown_flag = threading.Event()
-        
-        # Init queue in MongoDB server
-        self.queue = MongoQueue(collection=database.tasks,
-                                consumer_id=self.ident,
-                                max_attempts=1)
+        # When there is no job, take a timeout
+        self.timeout = timeout
+        # Jobs queue
+        self.queue = queue
 
     def run(self):
         """ Run work loop
         """
         print(f'Thread #{self.ident} started')
-        
+
         while not self.shutdown_flag.is_set():
             job = self.queue.next()
-            
+
             if job is None:
-                time.sleep(0.5)
-                continue
+                # time.sleep(self.timeout)
+                break
 
             try:
                 start_time = time.time()
-                
+
                 logging.info(f'Job id-{job.job_id} is started')
-                
+
                 session = Session(hostname=job.payload['hostname'],
                                   community=job.payload['community'],
                                   version=2,
                                   retries=1,
                                   timeout=3)
-                
+
                 for item in session.walk(job.payload['oids']):
                     pass
             except Exception as err:
@@ -51,9 +48,7 @@ class Worker(threading.Thread):
             else:
                 logging.info(f'Job id-{job.job_id} is complete in {time.time()-start_time:.2f} sec')            
                 job.complete()
-            finally:
-                print(f'Jobs left: {queue.size()}', end='\r')
-            
+
         print(f'Thread #{self.ident} stopped')
 
     def stop(self):
